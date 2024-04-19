36b4d5bfb58e8c56c8f4062363d517353e46617f
Brandon Alexander Burtchell <62345978+dukeofjukes@users.noreply.github.com>
2024-04-19 17:13:33 -0500

    Start textbooks page, add a json file database for now

--- academic_alley/app/textbooks/page.tsx
+++ academic_alley/app/textbooks/page.tsx
@@ -1,9 +1,29 @@
 import { title } from "@/components/primitives";
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import { Button } from "@nextui-org/button";
+import { promises as fs } from "fs";
 
-export default function ConnPage() {
-	return (
-		<div>
-			<h1 className={title()}>Textbooks</h1>
-		</div>
-	);
+export default async function TextbooksPage() {
+  const file = await fs.readFile(process.cwd() + "/app/dataset.json", "utf8");
+  const textbooks = JSON.parse(file).textbooks;
+
+  return (
+    <div>
+      <h1 className={title()}>Textbooks</h1>
+      {textbooks.map((book) => (
+        <Card className="max-w-[400px]">
+          <CardHeader className="flex gap-3">
+            <p>{book.title}</p>
+            <p>{book.author} </p>
+          </CardHeader>
+          <CardBody>
+            <p>${book.price}</p>
+          </CardBody>
+          <Button>
+            add to cart
+          </Button>
+        </Card>
+      ))}
+    </div>
+  );
 }
