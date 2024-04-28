5dcd909cba944da15c5132fee32fa74d5014dbd5
Andrew Scouten <yzb2@txstate.edu>
2024-04-28 03:27:00 -0500

    Default people page

--- academic_alley/app/people/page.tsx
+++ academic_alley/app/people/page.tsx
@@ -1,9 +1,49 @@
-import { title } from "@/components/primitives";
+"use client"
+import React from "react";
+import {
+	Divider,
+	Pagination,
+	Spacer, 
+	User
+} from "@nextui-org/react";
 
-export default function DocsPage() {
-	return (
-		<div>
-			<h1 className={title()}>People</h1>
-		</div>
-	);
+import { BorderedInput } from "@components/inputs"
+
+export default function PeoplePage() {
+	const [search, setSearch] = React.useState("")
+
+  return (
+		<>
+			<h4>So much! But it probably looks like this? A filter is def needed</h4>
+			<Spacer y={10}/>
+			<BorderedInput 
+				label="Search for people!"
+				setState={setSearch}/>
+			<Spacer y={10}/>
+			<Divider/>
+			<Spacer y={10}/>
+			<User   
+				name="Jane Doe"
+				description="Product Designer"
+				avatarProps={{
+					src: "https://i.pravatar.cc/150?u=a04258114e29026702d"
+				}}/>
+			<User   
+				name="Jane Doe"
+				description="Product Designer"
+				avatarProps={{
+					src: "https://i.pravatar.cc/150?u=a04258114e29026702d"
+				}}/>
+			<User   
+				name="Jane Doe"
+				description="Product Designer"
+				avatarProps={{
+					src: "https://i.pravatar.cc/150?u=a04258114e29026702d"
+				}}/>
+			<Spacer y={10}/>
+			<Divider/>
+			<Spacer y={10}/>
+			<Pagination total={10} initialPage={1} />
+		</>
+  );
 }
