d286854739d3ba807a8d49be40627afe930a1235
Andrew Scouten <yzb2@txstate.edu>
2024-04-28 02:53:27 -0500

    Basic dining plan idea
    Make them clickable, format better

--- academic_alley/app/dining/page.tsx
+++ academic_alley/app/dining/page.tsx
@@ -1,9 +1,52 @@
-import { title } from "@/components/primitives";
+import React from "react";
+import {
+	Card, 
+	CardHeader, 
+	CardBody, 
+	Image
+} from "@nextui-org/react";
 
-export default function AboutPage() {
+/*
+https://www.flaticon.com/free-icon/cereal_5371118?related_id=5473629&origin=search
+https://www.flaticon.com/free-icon/cutlery_3170733
+https://www.flaticon.com/free-icon/dish_1065715
+*/
+
+function MealCard({
+	header,
+	image,
+}) {
 	return (
-		<div>
-			<h1 className={title()}>Dining</h1>
-		</div>
+    <Card className="py-4">
+      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+        <p className="text-tiny uppercase font-bold">Daily Mix</p>
+        <small className="text-default-500">12 Tracks</small>
+        <h4 className="font-bold text-large">Frontend Radio</h4>
+      </CardHeader>
+      <CardBody className="overflow-visible py-2">
+        <Image
+          alt="Card background"
+          className="object-cover rounded-xl"
+          src={image}
+          width={500}
+        />
+      </CardBody>
+    </Card>
 	);
+}
+
+export default function DiningPage() {
+  return (
+		<div className="grid gap-2 grid-cols-3 grid-rows-1">
+			<MealCard 
+				header=""
+				image="/cereal.png"/>
+			<MealCard 
+				header=""
+				image="/dish.png"/>
+			<MealCard 
+				header=""
+				image="/cutlery.png"/>
+		</div>
+  );
 }
