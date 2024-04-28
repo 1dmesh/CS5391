e0ce3c18cb1b739d20402d72290ab5c13b381742
Andrew Scouten <yzb2@txstate.edu>
2024-04-28 02:20:24 -0500

    More basic account

--- academic_alley/app/account/page.js
+++ academic_alley/app/account/page.js
@@ -1,44 +1,46 @@
 "use client"
 import React from "react";
 import { Card, Avatar, Input, Button } from "@nextui-org/react";
+import { onAuthStateChanged } from "firebase/auth"
 
-import { userInstance } from "@/components/firebase"
+import { authInstance } from "@/components/firebase"
+import { FlatInput } from "@/components/inputs";
 
 export default function Account() {
-  const [email, setEmail] = React.useState()
+  const [user, setUser] = React.useState()
+
+  onAuthStateChanged(authInstance(), (u) => {
+    setUser(u)
+  });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     // Handle form submission logic here
   };
 
-  const userInfo = (key) => {
-    console.log(userInstance())
-    //console.log(userInstance()[key])
-    return userInstance()[key]
-  }
-
   return (
     <div style={{ height: '100%', width: '100%' }} className="flex flex-wrap content-center justify-center">
       <Card style={{ height: '80%', width: '80%' }}>
         <Avatar
           className="w-100 h-100 mt-10 text-large justify-center self-center "
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/c5938d67e24c517f0803cf6aaa1ead65938a1256ac256c73faa48491781d4b32?apiKey=6428492ccfa04507ac0d21f77fdf7e58&"
+          src={user?.photoURL}
           alt="User avatar"
         />
         <form className="ml-10 mr-10" onSubmit={handleSubmit}>
           <div className="flex gap-5 mt-20 max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
-            <Input
+            <FlatInput
               isRequired
+              isClearable
               type="email"
-              id="email"
-              name="email"
               label="Email"
-              placeholder={userInfo('email')}
-              onChange={(e) => setEmail(e.target.value)}/>
-            <Input
-            
-            />
+              placeholder={user?.email}
+              setState={() => console.log("not implemented")}/>
+            <FlatInput
+              isRequired
+              isClearable
+              label="Test"
+              placeholder={user?.displayName}
+              setState={() => console.log("not implemented")}/>
           </div>
           <div className="flex gap-5 mt-3.5 max-md:flex-wrap max-md:max-w-full">
             <Input
