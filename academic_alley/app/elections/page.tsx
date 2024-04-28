6d3cb47574b6435ef5058e49bffb2394a2ebf592
Andrew Scouten <yzb2@txstate.edu>
2024-04-28 03:18:32 -0500

    Transport template?

--- academic_alley/app/elections/page.tsx
+++ academic_alley/app/elections/page.tsx
@@ -1,6 +1,14 @@
+/*
+The website runs a poll to test whom users will vote for in a fictitious 
+election of the head of the student union. You can store the results 
+of the poll in a database and draw a bar chart of the results using 
+the image functions. Suppose the three candidates are: 
+John, Mary, and Susa
+*/
+import React from "react";
 import { title } from "@/components/primitives";
 
-export default function DocsPage() {
+export default function ElectionPage() {
 	return (
 		<div>
 			<h1 className={title()}>Elections</h1>
--- academic_alley/app/transportation/page.tsx
+++ academic_alley/app/transportation/page.tsx
@@ -1,9 +1,54 @@
-import { title } from "@/components/primitives";
+/*
+Images:
+https://www.flaticon.com/free-icon/bus-ticket_3205244?term=bus+ticket&page=1&position=5&origin=search&related_id=3205244
+https://www.flaticon.com/free-icon/bus_1036191
+*/
+"use client"
+import React from "react";
+import {
+	Accordion, 
+	AccordionItem,
+	Image,
+	Spacer,
+} from "@nextui-org/react";
 
-export default function BlogPage() {
-	return (
-		<div>
-			<h1 className={title()}>Transportation</h1>
-		</div>
-	);
+export default function TransportPage() {
+  return (
+		<>
+			<h4>TODO: Format, make them clickable for a "checkout". Maybe a modal?</h4>
+			<Spacer y={10}/>
+			<Accordion 
+				variant="bordered"
+				defaultExpandedKeys={["1"]}
+				style={{ height: '100%', width: '100%' }}>
+				<AccordionItem 
+					key="1" 
+					title="Bus Tickets"
+					aria-label="Bus Tickets" 
+					subtitle="Press to expand"
+					startContent={
+						<Image
+							alt="Image of a bus ticket"
+							className="object-cover rounded-xl"
+							src="/assets/transport/bus-ticket.png"
+							width={100}/>
+					}>
+						Bus Tickets!
+				</AccordionItem>
+				<AccordionItem
+					key="2"
+					title="Bus Cards"
+					aria-label="Bus Cards"
+					startContent={
+						<Image
+							alt="Image of a bus ticket"
+							className="object-cover rounded-xl"
+							src="/assets/transport/bus.png"
+							width={100}/>
+					}>
+					Bus Cards!
+				</AccordionItem>
+			</Accordion>
+		</>
+  );
 }
