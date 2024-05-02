1d76b987fb3e5e101cf0109e8f962520aa75e407
Andrew Scouten <yzb2@txstate.edu>
2024-05-02 14:38:18 -0500

    semi-perm profile state

--- academic_alley/app/account/page.js
+++ academic_alley/app/account/page.js
@@ -12,52 +12,82 @@ import { onAuthStateChanged } from "firebase/auth"
 import { authInstance, sendPasswordReset } from "@/components/firebase"
 import { BorderedInput } from "@/components/inputs";
 
+let userData = {
+  username: "cs5391",
+  email: "cs5391.252@gmail.com",
+  address: "601 University Dr",
+  city: "San Marcos",
+  state: "TX",
+  zipcode: 78666,
+  firstname: "Test",
+  lastname: "Account"
+}
+
 export default function Account() {
   const [user, setUser] = React.useState()
   const [email, setEmail] = React.useState()
-  const [address, setAddress] = React.useState()
   const [username, setUsername] = React.useState()
+  const [address, setAddress] = React.useState()
   const [city, setCity] = React.useState()
-  const [firstname, setFirstname] = React.useState()
   const [state, setState] = React.useState()
-  const [lastname, setLastname] = React.useState()
   const [zipcode, setZipcode] = React.useState()
+  const [firstname, setFirstname] = React.useState()
+  const [lastname, setLastname] = React.useState()
 
   onAuthStateChanged(authInstance(), (u) => {
     setUser(u)
   });
 
+  React.useEffect(() => {
+    userData['email'] = email
+    userData['username'] = username
+    userData['address'] = address
+    userData['city'] = city
+    userData['state'] = state
+    userData['zipcode'] = zipcode
+    userData['firstname'] = firstname
+    userData['lastname'] = lastname
+  }, [email, address, username, city, state, zipcode, firstname, lastname])
+
   const fields = [
     {
+      key: "email",
       label: "Email",
       type: "email",
       setState: setEmail
     },
     {
+      key: "address",
       label: "Address",
       setState: setAddress
     },
     {
+      key: "username",
       label: "Username",
       setState: setUsername
     },
     {
+      key: "city",
       label: "City",
       setState: setCity
     },
     {
+      key: "firstname",
       label: "First Name",
       setState: setFirstname
     },
     {
+      key: "state",
       label: "State",
       setState: setState
     },
     {
+      key: "lastname",
       label: "Last Name",
       setState: setLastname
     },
     {
+      key: "zipcode",
       label: "Zipcode",
       setState: setZipcode
     },
@@ -96,6 +126,7 @@ export default function Account() {
                   key={i}
                   type={field.type ? field.type : "text"}
                   label={field.label}
+                  defaultValue={userData[field.key]}
                   setState={field.setState}/>
               );
             })}
