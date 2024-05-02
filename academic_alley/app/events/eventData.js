daec495478e96e4adb48df7c4dd15d5805963313
Andrew Scouten <yzb2@txstate.edu>
2024-05-02 10:58:02 -0500

    Implement invents & make pretty

--- academic_alley/app/events/eventData.js
+++ academic_alley/app/events/eventData.js
@@ -1,70 +1,70 @@
 export const eventData = [
   {
     "year": 2024,
-    "month": "June",
+    "month": 6,
     "date": 15,
     "title": "Summer Solstice Art Fair",
     "description": "Celebrate the longest day of the year with our annual art fair featuring local artists, live music, and a variety of food vendors."
   },
   {
-    "year": 2023,
-    "month": "October",
+    "year": 2024,
+    "month": 10,
     "date": 31,
     "title": "Haunted Halloween Tour",
     "description": "Join us for a spooky tour through the old town, filled with ghost stories and haunted histories. Suitable for ages 12 and up."
   },
   {
     "year": 2024,
-    "month": "April",
+    "month": 4,
     "date": 22,
     "title": "Earth Day Clean-Up Initiative",
     "description": "Help make a difference by joining the community for a clean-up event at the local park. Supplies and refreshments provided."
   },
   {
     "year": 2025,
-    "month": "February",
+    "month": 2,
     "date": 14,
     "title": "Valentine's Day Couples Run",
     "description": "Lace up your running shoes with your significant other for a fun and romantic 5K through the scenic city park."
   },
   {
     "year": 2024,
-    "month": "August",
+    "month": 8,
     "date": 20,
     "title": "International Food Festival",
     "description": "Experience flavors from around the world at our annual food festival. Enjoy live music, cooking demonstrations, and delicious eats."
   },
   {
-    "year": 2023,
-    "month": "December",
+    "year": 2024,
+    "month": 12,
     "date": 25,
     "title": "Christmas Day Community Feast",
     "description": "Join us for a community feast open to everyone. Celebrate the holiday spirit with a delicious meal and festive entertainment."
   },
   {
     "year": 2024,
-    "month": "May",
-    "date": 5,
+    "month": 5,
+    "date": 10,
     "title": "Cinco de Mayo Street Parade",
     "description": "Experience the vibrant traditions of Cinco de Mayo with a colorful street parade, traditional music, and dance performances."
   },
   {
     "year": 2024,
-    "month": "November",
+    "month": 10,
     "date": 27,
     "title": "Thanksgiving Charity Marathon",
     "description": "Before the feast, run for a cause in our Thanksgiving Charity Marathon. All proceeds go to local food banks."
   },
   {
-    "year": 2023,
-    "month": "September",
+    "year": 2025,
+    "month": 9,
     "date": 11,
     "title": "Patriot Day Concert",
     "description": "Commemorate Patriot Day with a special concert featuring local bands, honoring the heroes and remembering those we lost."
   },
   {
     "year": 2025,
-    "month": "January",
+    "month": 1,
     "date": 1,
     "title": "New Year's Day Polar Plunge",
     "description": "Kick off the new year with a brisk plunge into the cold waters of our local lake. Hot cocoa and towels provided."
--- academic_alley/app/events/page.js
+++ academic_alley/app/events/page.js
@@ -12,19 +12,47 @@ import { useDateFormatter } from "@react-aria/i18n";
 
 import { eventData } from "@/app/events/eventData"
 
+const MONTHS = {
+  Jan: '01',
+  Feb: '02',
+  Mar: '03',
+  Apr: '04',
+  May: '05',
+  Jun: '06',
+  Jul: '07',
+  Aug: '08',
+  Sep: '09',
+  Oct: '10',
+  Nov: '11',
+  Dec: '12',
+}
+
 export default function Page() {
   let formatter = useDateFormatter({dateStyle: "long"});
-	const [duration, setDuration] = React.useState()
+	const [duration, setDuration] = React.useState(null)
 
 	const durationChange = (d) => {
-		console.log(formatter.formatRangeToParts(
+		let parts = formatter.formatRangeToParts(
 			d.start.toDate(getLocalTimeZone()),
 			d.end.toDate(getLocalTimeZone()),
-		))
-		setDuration(formatter.formatRangeToParts(
-			d.start.toDate(getLocalTimeZone()),
-			d.end.toDate(getLocalTimeZone()),
-		))
+		)
+		if (parts.length === 7) {
+			setDuration({
+				month1: Number(MONTHS[parts[0].value]),
+				day1: Number(parts[2].value),
+				month2: Number(MONTHS[parts[0].value]),
+				day2: Number(parts[4].value),
+				year: Number(parts[6].value)
+			})
+		} else if (parts.length === 9) {
+			setDuration({
+				month1: Number(MONTHS[parts[0].value]),
+				day1: Number(parts[2].value),
+				month2: Number(MONTHS[parts[4].value]),
+				day2: Number(parts[6].value),
+				year: Number(parts[8].value)
+			})
+		}
 	}
 
 	return (
@@ -48,17 +76,37 @@ export default function Page() {
       <Spacer y={10}/>
       <Divider/>
       <Spacer y={5}/>
-			<div className="grid grid-cols-3">
+			<div className="grid grid-cols-3 gap-4">
 				{
 					eventData.map((e, i) => {
-						if(duration != null) {
-							console.log(typeof(e.year))
-							console.log(typeof(duration[6].value))
-						}
-						if (duration != null && e.year === Number(duration[6].value)) {
-								return <Card key={i}>
-									<h1>{e.title}</h1>
+						if (duration === null) {
+							return <Card key={i}>
+								<Spacer y={2}/>
+								<div className="p-4">
+									<h1>{e.title} ({e.month}-{e.date}-{e.year})</h1>
+									<Spacer y={2}/>
+									<Divider/>
+									<Spacer y={2}/>
 									<h2>{e.description}</h2>
+								</div>
+								<Spacer y={2}/>
+							</Card>
+						} else if (duration != null && 
+							e.year === duration.year &&
+							e.month >= duration.month1 &&
+							e.month <= duration.month2 &&
+							e.date >= duration.day1 &&
+							e.date <= duration.day2) {
+								return <Card key={i}>
+									<Spacer y={2}/>
+									<div className="p-4">
+										<h1>{e.title} ({e.month}-{e.date}-{e.year})</h1>
+										<Spacer y={2}/>
+										<Divider/>
+										<Spacer y={2}/>
+										<h2>{e.description}</h2>
+									</div>
+									<Spacer y={2}/>
 								</Card>
 						}
 						return <React.Fragment key={i}/>
